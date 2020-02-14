ec067eb0b52cb9704776ce4f91d94df72edbff10
Jenny <1305633200@qq>
2020-02-14 17:57:34 +0800

    ADD report

--- test_suite.py
+++ test_suite.py
@@ -2,8 +2,10 @@ import unittest
 
 from case.test_login import TestLogin
 from case.test_user_file import TestUserFile
+from tools.HTMLTestRunner import HTMLTestRunner
 
 suite = unittest.TestSuite()
 suite.addTest(unittest.makeSuite(TestLogin))
 suite.addTest(unittest.makeSuite(TestUserFile))
-unittest.TextTestRunner(verbosity=2).run(suite)
+with open("./report/report.html","wb") as f:
+   HTMLTestRunner(stream=f).run(suite)
